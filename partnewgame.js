6a26e40d7aad39cadc182619a51c2ac5beadfec6
Cyan00ffff <50438923+Cyan00ffff@users.noreply.github.com>
2019-06-19 16:08:32 +0900

    디자인 제이슨 수정!

--- partnewgame.js
+++ partnewgame.js
@@ -5,6 +5,7 @@
 var string_title1=new Array;
 var string_thumbnail=new Array;
 var string_content=new Array;
+var string_pic=new Array;
 
 
 
@@ -16,6 +17,7 @@ $(function(){
 	string_title=getJson_title("content_2000");
 	string_thumbnail=getJson_content("content_2000");
 	string_content=getJson_cURL("content_2000");
+	string_pic=getJson_cPIC("content_2000");
 
 	$("#article_title1_1").html(string_title[0]);
 	$("#article_thumbnail1_1").html(string_thumbnail[0][1]);
@@ -34,41 +36,84 @@ $(function(){
 	$("#article_content1_4").html(string_content[3]);
 
 
+
+	//여기서부터 partition2
+
 	$("#article_title2-1").html(string_title[4]);
-	$("#content2-1-1-title").html(string_content[4][0]);
-	// $("#content2-1-1-body")
-	$("#introduce1").html(string_content[4][3]);
-	$("#content2-1-2-title").html(string_content[4][1]);
-	$("#content2-1-3-title").html(string_content[4][2]);
+	$("#content2-1-1-title").html(string_thumbnail[4][0]);
+	$("#article_img2-1-1").attr("src",string_pic[4][0]);
+	$("#article_link2-1-1").attr("href",string_pic[4][6]);
+	$("#article_link2-1-2").attr("href",string_pic[4][7]);
+	$("#article_link2-1-3").attr("href",string_pic[4][8]);
+	$("#article_link2-1-4").attr("href",string_pic[4][9]);
+	$("#article_link2-1-5").attr("href",string_pic[4][10]);
+	$("#article_link2-1-6").attr("href",string_pic[4][11]);
+
+	$("#article_img2-1-2").attr("src",string_pic[4][1]);
+	$("#article_img2-1-3").attr("src",string_pic[4][2]);
+	$("#article_img2-1-4").attr("src",string_pic[4][3]);
+	$("#article_img2-1-5").attr("src",string_pic[4][4]);
+	$("#article_img2-1-6").attr("src",string_pic[4][5]);
+
+	$("#introduce1").html(string_thumbnail[4][3]);
+	
+	$("#content2-1-2-title").html(string_thumbnail[4][1]);
+	$("#content2-1-3-title").html(string_thumbnail[4][2]);
 	// $("#content2-1-4-title").html(string_content[4][3]);
 	// $("#content2-1-5-title").html(string_content[4][4]);
 
 	$("#article_title2-2").html(string_title[5]);
-	$("#content2-2-1-title").html(string_content[5][0]);
-	// $("#content2-1-1-body")
-	$("#introduce2").html(string_content[5][4]);
-	$("#content2-2-2-title").html(string_content[5][1]);
-	$("#content2-2-3-title").html(string_content[5][2]);
-	$("#content2-2-4-title").html(string_content[5][3]);
-	// $("#content2-2-5-title").html(string_content[5][4]);
+	$("#content2-2-1-title").html(string_thumbnail[5][0]);
+	$("#article_img2-2-1").attr("src",string_pic[5][0]);
+	$("#article_img2-2-2").attr("src",string_pic[5][1]);
+	$("#article_img2-2-3").attr("src",string_pic[5][2]);
+	$("#article_img2-2-4").attr("src",string_pic[5][3]);
+	$("#article_img2-2-5").attr("src",string_pic[5][4]);
+	$("#article_img2-2-6").attr("src",string_pic[5][5]);
+	$("#article_link2-2-1").attr("href",string_pic[5][6]);
+	$("#article_link2-2-2").attr("href",string_pic[5][7]);
+	$("#article_link2-2-3").attr("href",string_pic[5][8]);
+	$("#article_link2-2-4").attr("href",string_pic[5][9]);
+	$("#article_link2-2-5").attr("href",string_pic[5][10]);
+	$("#article_link2-2-6").attr("href",string_pic[5][11]);
+
+	$("#introduce2").html(string_thumbnail[5][4]);
+	$("#content2-2-2-title").html(string_thumbnail[5][1]);
+	$("#content2-2-3-title").html(string_thumbnail[5][2]);
+	$("#content2-2-4-title").html(string_thumbnail[5][3]);
 
 	$("#article_title2-3").html(string_title[6]);
-	$("#content2-3-1-title").html(string_content[6][0]);
-	// $("#content2-1-1-body")
-	$("#introduce3").html(string_content[6][1]);
-	// $("#content2-3-2-title").html(string_content[6][1]);
-	// $("#content2-3-3-title").html(string_content[6][2]);
-	// $("#content2-3-4-title").html(string_content[6][3]);
-	// $("#content2-3-5-title").html(string_content[6][4]);
+	$("#content2-3-1-title").html(string_thumbnail[6][0]);
+	$("#introduce3").html(string_thumbnail[6][1]);
+	$("#article_img2-3-1").attr("src",string_pic[6][0]);
+	$("#article_img2-3-2").attr("src",string_pic[6][1]);
+	$("#article_img2-3-3").attr("src",string_pic[6][2]);
+	$("#article_img2-3-4").attr("src",string_pic[6][3]);
+	$("#article_img2-3-5").attr("src",string_pic[6][4]);
+	// $("#article_img2-3-6").attr("src",string_pic[6][5]);
+	$("#article_link2-3-1").attr("href",string_pic[6][5]);
+	$("#article_link2-3-2").attr("href",string_pic[6][6]);
+	$("#article_link2-3-3").attr("href",string_pic[6][7]);
+	$("#article_link2-3-4").attr("href",string_pic[6][8]);
+	$("#article_link2-3-5").attr("href",string_pic[6][9]);
+	// $("#article_link2-3-6").attr("href",string_pic[6][10]);
 
+	
 	$("#article_title2-4").html(string_title[7]);
-	$("#content2-4-1-title").html(string_content[7][0]);
-	// $("#content2-1-1-body")
-	$("#introduce4").html(string_content[7][1]);
-	// $("#content2-4-2-title").html(string_content[7][1]);
-	// $("#content2-4-3-title").html(string_content[7][2]);
-	// $("#content2-4-4-title").html(string_content[7][3]);
-	// $("#content2-4-5-title").html(string_content[7][4]);
+	$("#content2-4-1-title").html(string_thumbnail[7][0]);
+	$("#introduce4").html(string_thumbnail[7][1]);
+	$("#article_img2-4-1").attr("src",string_pic[7][0]);
+	$("#article_img2-4-2").attr("src",string_pic[7][1]);
+	$("#article_img2-4-3").attr("src",string_pic[7][2]);
+	$("#article_img2-4-4").attr("src",string_pic[7][3]);
+	$("#article_img2-4-5").attr("src",string_pic[7][4]);
+	// $("#article_img2-4-6").attr("src",string_pic[7][5]);
+	$("#article_link2-4-1").attr("href",string_pic[7][5]);
+	$("#article_link2-4-2").attr("href",string_pic[7][6]);
+	$("#article_link2-4-3").attr("href",string_pic[7][7]);
+	$("#article_link2-4-4").attr("href",string_pic[7][8]);
+	$("#article_link2-4-5").attr("href",string_pic[7][9]);
+
 
 
 	$("#article_title3_1").html(string_title[8]);
