6bcba6a5758217b403bb23334434b9a1e321aecc
kukxzl <xiazilei1994@gmail.com>
2019-06-19 14:27:41 +0800

    re commit
    re commit

--- partnewgame.js
+++ partnewgame.js
@@ -8,6 +8,7 @@ var string_content=new Array;
 var string_pic=new Array;
 
 
+
 $(function(){
 
 	initJson("content_2000");
@@ -83,19 +84,20 @@ $(function(){
 	$("#article_content3_1").html(string_content[8]);
 
 
-	$("#article_title3_1").html(string_title[9]);
-	$("#article_thumbnail3_1").html(string_thumbnail[9][0]);
-	$("#article_content3_1").html(string_content[9]);
+	$("#article_title3_2").html(string_title[9]);
+	$("#article_thumbnail3_2").append(string_thumbnail[9][0]);
+	// $("#article_content3_2").html(string_pic[9][0]);
+	$("#article_content3_2").attr("src",string_pic[9][0]);
 
+	$("#article_title3_3").html(string_title[10]);
+	$("#article_thumbnail3_3").append(string_thumbnail[10][0]);
+	// $("#article_content3_3").html(string_content[10]);
+	$("#article_content3_3").attr("src",string_pic[10][0]);
 
-	$("#article_title3_1").html(string_title[10]);
-	$("#article_thumbnail3_1").html(string_thumbnail[10][0]);
-	$("#article_content3_1").html(string_content[10]);
-
-
-	$("#article_title3_1").html(string_title[11]);
-	$("#article_thumbnail3_1").html(string_thumbnail[11][0]);
-	$("#article_content3_1").html(string_content[11]);
+	$("#article_title3_4").html(string_title[11]);
+	$("#article_thumbnail3_4").append(string_thumbnail[11][0]);
+	// $("#article_content3_4").html(string_content[11]);
+	$("#article_content3_4").attr("src",string_pic[11][0]);
 	},2000);
 
 	
