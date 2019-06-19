a098b22e9f02546a573b9bb6be15dc448e61b0de
kukxzl <xiazilei1994@gmail.com>
2019-06-19 14:06:02 +0800

    new game finished
    new game imgs added

--- partnewgame.js
+++ partnewgame.js
@@ -8,7 +8,6 @@ var string_content=new Array;
 var string_pic=new Array;
 
 
-
 $(function(){
 
 	initJson("content_2000");
@@ -19,22 +18,27 @@ $(function(){
 	string_content=getJson_cURL("content_2000");
 	string_pic=getJson_cPIC("content_2000");
 
-
 	$("#article_title1_1").html(string_title[0]);
-	$("#article_thumbnail1_1").html(string_thumbnail[0][1]);
+	//$("#article_thumbnail1_1").html(string_thumbnail[0][0]);
 	$("#article_content1_1").html(string_content[0]);
+	$("#article_thumbnail1_1").css({"background":"url('/NewGame_imgs/card0.jpg')","background-size":"100%"});
+
 
 	$("#article_title1_2").html(string_title[1]);
-	$("#article_thumbnail1_2").html(string_thumbnail[1][0]);
+	//$("#article_thumbnail1_2").html(string_thumbnail[1][0]);
 	$("#article_content1_2").html(string_content[1]);
+	$("#article_thumbnail1_2").css({"background":"url('/NewGame_imgs/card1_1.jpg')","background-size":"100%"});
+
 
 	$("#article_title1_3").html(string_title[2]);
-	$("#article_thumbnail1_3").html(string_thumbnail[2][1]);
+	//$("#article_thumbnail1_3").html(string_thumbnail[2][1]);
 	$("#article_content1_3").html(string_content[2]);
+	$("#article_thumbnail1_3").css({"background":"url('/NewGame_imgs/card2_1.jpg')","background-size":"100%"});
 
 	$("#article_title1_4").html(string_title[3]);
-	$("#article_thumbnail1_4").html(string_thumbnail[3][0]);
+	//$("#article_thumbnail1_4").html(string_thumbnail[3][0]);
 	$("#article_content1_4").html(string_content[3]);
+	$("#article_thumbnail1_4").css({"background":"url('/NewGame_imgs/card3_1.jpg')","background-size":"100%"});
 
 
 	$("#article_title2-1").html(string_title[4]);
@@ -79,20 +83,19 @@ $(function(){
 	$("#article_content3_1").html(string_content[8]);
 
 
-	$("#article_title3_2").html(string_title[9]);
-	$("#article_thumbnail3_2").append(string_thumbnail[9][0]);
-	// $("#article_content3_2").html(string_pic[9][0]);
-	$("#article_content3_2").attr("src",string_pic[9][0]);
+	$("#article_title3_1").html(string_title[9]);
+	$("#article_thumbnail3_1").html(string_thumbnail[9][0]);
+	$("#article_content3_1").html(string_content[9]);
 
-	$("#article_title3_3").html(string_title[10]);
-	$("#article_thumbnail3_3").append(string_thumbnail[10][0]);
-	// $("#article_content3_3").html(string_content[10]);
-	$("#article_content3_3").attr("src",string_pic[10][0]);
 
-	$("#article_title3_4").html(string_title[11]);
-	$("#article_thumbnail3_4").append(string_thumbnail[11][0]);
-	// $("#article_content3_4").html(string_content[11]);
-	$("#article_content3_4").attr("src",string_pic[11][0]);
+	$("#article_title3_1").html(string_title[10]);
+	$("#article_thumbnail3_1").html(string_thumbnail[10][0]);
+	$("#article_content3_1").html(string_content[10]);
+
+
+	$("#article_title3_1").html(string_title[11]);
+	$("#article_thumbnail3_1").html(string_thumbnail[11][0]);
+	$("#article_content3_1").html(string_content[11]);
 	},2000);
 
 	
